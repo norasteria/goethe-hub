8a9e1ccfeb18389f7af5fcf0204adcbf8f372cc0
noraasteria <nora_asteria@yahoo.cp.od>
2025-01-19 12:40:45 +0700

    Rename project

--- src/main/java/com/noralearn/usermanagment/GoetheHubApplication.java
+++ src/main/java/com/noralearn/usermanagment/GoetheHubApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UsermanagmentApplication {
+public class GoetheHubApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UsermanagmentApplication.class, args);
+		SpringApplication.run(GoetheHubApplication.class, args);
 	}
 
 }
