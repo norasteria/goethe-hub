3184da4f2c4aa09eb6fe36aa4cbcb9313f9b427f
Nora Asteria <nora_asteria@yahoo.co.id>
2025-01-19 13:31:49 +0700

    Rename project (#2)
    Co-authored-by: noraasteria <nora_asteria@yahoo.cp.od>

--- src/test/java/com/noralearn/usermanagment/GoetheHubApplicationTests.java
+++ src/test/java/com/noralearn/usermanagment/GoetheHubApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class UsermanagmentApplicationTests {
+class GoetheHubApplicationTests {
 
 	@Test
 	void contextLoads() {
